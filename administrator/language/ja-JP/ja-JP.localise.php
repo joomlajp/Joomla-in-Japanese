e117a43ba5ac2908d32fe597b156b9f30a44c16d
Richell <phatstyle@gmail.com>
2013-04-16 12:42:30 +0900

    updated for 3.1.0.1

--- administrator/language/ja-JP/ja-JP.localise.php
+++ administrator/language/ja-JP/ja-JP.localise.php
@@ -14,7 +14,7 @@ defined('_JEXEC') or die;
  * @package  Joomla.Language
  * @since    1.6
  */
-abstract class ja_JPLocalise
+abstract class Ja_JPLocalise
 {
 	/**
 	 * Returns the potential suffixes for a specific number of items
